e619b18a893ac07c1cfacdbc7122895048a52805
Bryan Leighton <bryan.leigh77@gmail.com>
2018-12-11 16:35:23 +0200

    add red filter

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -30,8 +30,23 @@ function paintingToCanvas(){
 	
 	setInterval(() => {
 		 ctx.drawImage(video, 0, 0, width, height);
-		 // const pixels = ctx.getImageData(0, 0, width, height);
-	}, 16)
+		 //  take pixels out
+		 let pixels = ctx.getImageData(0, 0, width, height);
+		 // mess with them
+		 pixels = redEffect(pixels);
+
+		 //  put them back
+		 ctx.putImageData(pixels, 0, 0);
+	}, 16);
+}
+
+function redEffect(pixels){
+	for(let i = 0; i < pixels.data.length; i+=4) {
+		pixels.data[i + 0] = pixels.data[i + 0] + 100;  //Red
+		pixels.data[i + 1] = pixels.data[i + 1] - 50; //Green
+		pixels.data[i + 2] = pixels.data[i + 2] * 0.5; //Blue
+	}
+	return pixels;
 }
 
 function takePhoto(){
