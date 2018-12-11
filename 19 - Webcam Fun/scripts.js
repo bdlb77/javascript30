f51df1403fd0a1ab7049b76e724a7946b47bc52f
Bryan Leighton <bryan.leigh77@gmail.com>
2018-12-11 17:04:18 +0200

    19- webcam greenscreen

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -28,13 +28,14 @@ function paintingToCanvas(){
 	canvas.width = width;
 	canvas.height = height;
 	
-	setInterval(() => {
+	return setInterval(() => {
 			ctx.drawImage(video, 0, 0, width, height);
 			//  take pixels out
 			let pixels = ctx.getImageData(0, 0, width, height);
 			// mess with them
 			// pixels = redEffect(pixels);
-			pixels = rgbSplit(pixels);
+			pixels = greenScreen(pixels);
+			// ctx.globalAlpha = 0.2;
 		 //  put them back
 		 ctx.putImageData(pixels, 0, 0);
 	}, 16);
@@ -75,6 +76,31 @@ function rgbSplit(pixels){
 	return pixels;
 }
 
-video.addEventListener('canplay', paintingToCanvas);
+function greenScreen(pixels){
+	const levels = {};
+	document.querySelectorAll('.rgb input').forEach((input) => {
+		levels[input.name] = input.value;
+	});
+
+ for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (red >= levels.rmin
+      && green >= levels.gmin
+      && blue >= levels.bmin
+      && red <= levels.rmax
+      && green <= levels.gmax
+      && blue <= levels.bmax) {
+      // take it out!
+      pixels.data[i + 3] = 0;
+    }
+	}
+	return pixels
+
+}
 
 getVideo();
+video.addEventListener('canplay', paintingToCanvas);
